68d884e72c912d3b8d90fa5a9d174163d0b9b5e9
Muhammad Isfaaq Goomany <isfaaqg@gmail.com>
2021-04-11 13:51:50 +0400

    wrapper for squash rot

--- lib/wrappers.py
+++ lib/wrappers.py
@@ -356,6 +356,38 @@ class ProcessFrame84Squash(gym.ObservationWrapper):
 
         return frame
 
+
+class ProcessFrame84SquashRot(gym.ObservationWrapper):
+    def __init__(self, env=None):
+        super(ProcessFrame84SquashRot, self).__init__(env)
+        self.observation_space = gym.spaces.Box(
+            low=0, high=255, shape=(84, 84, 1), dtype=np.uint8)
+
+    def observation(self, obs):
+        return ProcessFrame84SquashRot.process(obs)
+
+    @staticmethod
+    def process(frame):
+        if frame.size == 210 * 160 * 3:
+            img = np.reshape(frame, [210, 160, 3]).astype(np.float32)
+        elif frame.size == 250 * 160 * 3:
+            img = np.reshape(frame, [250, 160, 3]).astype(np.float32)
+        else:
+            assert False, "Unknown resolution."
+        # grayscale
+        img = img[:, :, 0] * 0.299 + img[:, :, 1] * 0.587 + img[:, :, 2] * 0.114
+        # crop
+        img = img[34:, 8:152]
+        # rotate
+        img = np.rot90(img, 1)
+        # resize
+        img = cv2.resize(img, (84, 84), interpolation=cv2.INTER_AREA)
+        # Add 3rd channel
+        img = np.reshape(img, [84, 84, 1])
+        return img.astype(np.uint8)
+
+        return frame
+
 def make_env_po(env_name):
     env = gym.make(env_name)
     env = MaxAndSkipEnv(env)
@@ -407,6 +439,15 @@ def make_env_sq(env_name):
     return env
 
 
+def make_env_sq_rot(env_name):
+    env = gym.make(env_name)
+    env = MaxAndSkipEnv(env)
+    env = ProcessFrame84SquashRot(env)
+    env = ImageToPyTorch(env)
+    env = BufferWrapper(env, 4)
+    env = ScaledFloatFrame(env)
+    return env
+
 def get_env(num):
     if num == 1:  # Pong
         env = make_env_po('PongNoFrameskip-v4')
@@ -423,18 +464,23 @@ def get_env(num):
     elif num == 5:  # Squash
         env = make_env_sq('Squash-v0')
         wr = "Squash"  
+    elif num == 6:  # Squash Rotated
+        env = make_env_sq_rot('Squash-v0')
+        wr = "Squash"  
         
     return wr, env
 
 if __name__ == '__main__':
     env = gym.make('Squash-v0')
+    env = ProcessFrame84SquashRot(env)
     env.reset()
     for i in range(100):
         o, r, d, i = env.step(env.action_space.sample())
         # env.render()
         if d:
             env.reset()
-    f = ProcessFrame84Squash.process(o)
+    # f = ProcessFrame84SquashRot.process(o)
+    f = o
     
     cv2.imshow("out", f)
     # env = gym.make('Squash-v0')
